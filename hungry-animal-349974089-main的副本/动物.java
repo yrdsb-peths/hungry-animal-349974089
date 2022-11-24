e3289f2db6723ca2c79cdf9bc0cd96043958d421
349974089 <117937232+349974089@users.noreply.github.com>
2022-11-24 17:15:15 -0500

    Add files via upload

---
+++ hungry-animal-349974089-main的副本/动物.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class 动物 here.
+ * 
+ * @author Rex 
+ * @version 2022/11/24
+ */
+public class 动物 extends Actor
+{
+    /**
+     * Act - do whatever the 动物 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+    }
+}
