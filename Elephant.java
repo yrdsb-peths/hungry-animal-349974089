525b9b65ec581e90d7be877a90453b44689941cd
349974089 <117937232+349974089@users.noreply.github.com>
2022-12-11 11:10:44 -0500

    Add files via upload

---
+++ Elephant.java
@@ -0,0 +1,81 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class 熊 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Elephant extends Actor
+{
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    public Elephant()
+    {
+        for(int i=0; i<idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100,100);
+        }
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100,100);
+        }
+        
+        animationTimer.mark();
+        setImage(idleRight[0]);
+    }
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    public void act()
+    {
+        
+        if(Greenfoot.isKeyDown("Left"))
+        {
+            move(-2);
+            facing = "left";
+        }
+        if(Greenfoot.isKeyDown("Right"))
+        {
+            move(2);
+            facing = "right";
+        }
+        
+        eat();
+        
+        animateElephant();
+    }
+    public void eat()
+    {
+        if(isTouching(苹果.class))
+        {
+            removeTouching(苹果.class);
+            MyWorld world = (MyWorld)getWorld();
+            world.creatApple();
+            world.increaseScore();
+            elephantSound.play();
+        }
+    }
+}
